1972d189766b153aa6f4b4087219c29ff21cf859
Anjali Dube <anjali.dube27@gmail.com>
2019-04-28 13:00:07 +0200

    Pagination For 25 Contacts added

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -7,22 +7,35 @@ class Contact extends React.Component {
     super(props)
 
     this.state = {
-      contacts: []
+      contacts: [],
+      currentPage: 1,
+      countPerPage: 25,
+      activeIndex: 1 
     }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(event,index) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+
+    //Update Active Page Index
+    this.setState({ activeIndex: index });
   }
 
   componentDidMount() {
-
     const auth = {
-      headers: { "authToken": localStorage.getItem('authtoken') }
+      headers: { "authToken": localStorage.getItem('authtoken') },
     }
-    axios.get('https://internal-api-staging-lb.interact.io/v2/lists', auth)
+
+   axios.get('https://internal-api-staging-lb.interact.io/v2/lists?', auth)
       .then(response => {
         const listId = response.data[0].id
         if (listId) {
-          axios.get(`https://internal-api-staging-lb.interact.io/v2/lists/${listId}/contacts`, auth)
+          axios.get(`https://internal-api-staging-lb.interact.io/v2/lists/${listId}/contacts?limit=100`, auth)
             .then(responseContacts => {
               this.setState({ contacts: responseContacts.data.data })
+              console.log(this.state.contacts);
             })
             .catch((err) => {
               console.log(err)
@@ -43,8 +56,36 @@ class Contact extends React.Component {
   }
 
   render() {
+    const { contacts,currentPage, countPerPage } = this.state;
+
+    // Logic for displaying Per Page counts
+    const indexOfLastCount = currentPage * countPerPage;
+    const indexOfFirstCount = indexOfLastCount - countPerPage;
+    const currentCounts = contacts.slice(indexOfFirstCount, indexOfLastCount);
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(contacts.length / countPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+
+    // Render Page Numbers
+    const renderPageNumbers = pageNumbers.map(number => {
+        const className = this.state.activeIndex === number ? 'active' : '';
+        return (
+          <li
+            key={number}
+            id={number}
+            className={className}
+            onClick={ (event) => this.handleClick(event,number)}>
+            {number}
+          </li>
+        );
+      });
+    
     return (
-      <div>
+       <div>
         <nav className="navbar navbar-light bg-light">
           <div className="d-flex justify-content-start align-items-center">
             <span className="navbar-brand">CRM</span>
@@ -63,8 +104,8 @@ class Contact extends React.Component {
                 </tr>
               </thead>
               <tbody>
-                {this.state.contacts &&
-                  this.state.contacts.map((contact, index) => (
+                {currentCounts &&
+                  currentCounts.map((contact, index) => (
                     <tr key={index}>
                       <td >{index+1}</td>
                       <td>{contact.contactType === "COMPANY" ? contact.companyName : contact.firstName+" "+contact.lastName}</td>
@@ -74,10 +115,14 @@ class Contact extends React.Component {
                   ))}
               </tbody>
             </table>
+            <ul id="page-numbers" className="float-right">
+                {renderPageNumbers}
+            </ul>
           </div>
         </div>
       </div>
     )
   }
 }
-export default Contact
+
+export default Contact;
